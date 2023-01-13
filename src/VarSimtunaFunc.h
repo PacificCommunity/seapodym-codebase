aa174fe150c93e33a6e389a2017cdda049441e68
innas@tiny <inna.senina@gmail.com>
2023-01-13 18:10:13 +0100

    SimtunaFunc cleanup

--- src/VarSimtunaFunc.h
+++ src/VarSimtunaFunc.h
@@ -17,16 +17,6 @@ public:
 
 public:
 
-	//admodel functions
-//	void spawning_habitat(VarParamCoupled& param, CMatrices& mat, const PMap& map, dvar_matrix& H, int sp);
-//	DOUBLE Juvenile_habitat(VarParamCoupled& param, CMatrices& mat, int i, int j, int sp);
-//	void mortality_sp(VarParamCoupled& param, const PMap& map, dvar_matrix& M, dvar_matrix H, const int sp, const double mean_age_in_month);
-//	dvariable f_accessibility(VarParamCoupled& param, CMatrices& mat, dvariable teta, int i, int j, int sp, int age, int nf);
-//	void feeding_habitat(VarParamCoupled& param, CMatrices& mat, const PMap& map, dvar_matrix& Ha, int sp, int age);
-
-	//void nF_ratio_comp(VarParamCoupled& param, CMatrices& mat, const PMap& map, const int sp, const int age, int i, int j);
-	//void layer_access_comp(VarParamCoupled& param, CMatrices& mat, PMap& map, const int sp, const int age);
-
 	//adjoint code
 	void Spawning_Habitat(VarParamCoupled& param, CMatrices& mat, const PMap& map, dvar_matrix& Hs, const double sigma_sp_var, int sp, const int t_count, const int jday);
 	void Hs_comp(VarParamCoupled& param, CMatrices& mat, const PMap& map, dvar_matrix& Hs, double a, double b, double c, double d, double e, const double sigma_sp_var, const int jday, int t_count);
@@ -63,9 +53,6 @@ public:
 	void M_sp_comp(const PMap& map, dvar_matrix& M, const dmatrix& H, double, double, double, double, double, double, const int dtau);
 	void M_PH_juv_comp(VarParamCoupled& param, const PMap& map, CMatrices& mat, dvar_matrix& M, const dmatrix& PH, double mean_age_in_dtau);
 
-	//void Diff_func(const PMap& map, dvar_matrix& F, dvar_matrix& Ha, dvariable& c_adv_fish, dvariable& chi_species);
-	//void diff_func_comp(const PMap& map, dmatrix& F, dmatrix& H, double c, double move_par);
-
 	void allocate_dvmatr(const int imin, const int imax, const ivector jinf, const ivector jsup){
 		dvmatr1.allocate(imin,imax,jinf,jsup); dvmatr1.initialize();
 		dvmatr2.allocate(imin,imax,jinf,jsup); dvmatr2.initialize();
@@ -84,7 +71,7 @@ public:
 
 	double elapsed_time_reading;
 	void time_reading_init() {elapsed_time_reading = 0;}
-	//double get_time_reading() {return elapsed_time_reading;}
+
 private:
 	dvar_matrix dvmatr1, dvmatr2, dvmatr3, dvmatr4, dvmatr5, dvmatr6, dvmatr7, dvmatr8;
 
