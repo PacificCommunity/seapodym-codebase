61711fe58457253986f41df46d8d070f4f506af0
isenina <innas@spc.int>
2025-02-26 13:57:37 +0100

    Update dym library
    This update includes a fix of function 'read.var.dym' to enable
    extraction of a transect without loosing the 3D dimension of the output.
    Also, some more checks and functionality added to the date extraction.
    Namely, if either of t0.user or tfin.user are not specified, the data
    from the first date or until the last date respectively will be
    extracted. Also, function mult.dyms was added to the tookbox.

---
+++ rtools/dym/R/read.R
@@ -0,0 +1,322 @@
+#20170515: Function to read the data from DYM files with R functions for binary files
+# **********************************************************************************************************
+
+	xtoi.dym<-function(x,xmin,dx) {
+		return(round((x-xmin)/dx,digits=0)+1)
+	}
+
+	ytoj.dym<-function(y,ymin,dy){
+		return(round((ymin-y)/dy,digits=0)+1)
+	}
+
+	ymd.2date <- function(y,m,d) as.Date(paste(y,m,d,sep="-"))
+	date.2ymd <- function(d) as.integer(c(format(d,"%Y"),format(d,"%m"),format(d,"%d")))
+
+	year.month.sea<-function(ndat){
+		year <- trunc(ndat)
+		days <- trunc((ndat - year)*365);
+		date <- ymd.2date(year,1,1)+days-1
+		month<-as.integer(format(date,"%m"))
+		return(c(year,month))
+	}
+
+	gen.monthly.dates<-function(t0,tfin){
+		days<-seq(ymd.2date(t0[1],t0[2],15),ymd.2date(tfin[1],tfin[2],15),1)
+		days15<-days[which(as.numeric(format(days,"%d"))==15)]
+		return(days15)
+	}
+
+	read.dims.dym<-function(file.in){
+
+	#1-reading
+		message("Reading dimensions from file ",file.in)		
+		con<-file(file.in,"rb") 
+		file.type<-readChar(con,4)
+		grid.id<-readBin(con,integer(),size=4)
+		minval<-readBin(con,numeric(),size=4)
+		maxval<-readBin(con,numeric(),size=4)
+		nlon<-readBin(con,integer(),size=4)
+		nlat<-readBin(con,integer(),size=4)
+		nlevel<-readBin(con,integer(),size=4)
+		close(con)
+		return(list(nt=nlevel,nx=nlon,ny=nlat))
+	}
+
+	#' Reads dimensional vectors from the DYM file
+	#'
+	#' @param file.in the name of the DYM file.
+	#' @return A list with t, x and y, where time vector returned in decimal format, as written in the DYM file, x and y are the longitudinal and latitudinal vectors respectively.
+	#' @examples 
+	#' dym <- read.txy.dym("skj_adult.dym");
+	#' summary(dym)
+	#' @export
+	read.txy.dym<-function(file.in,verbose=FALSE){
+
+		#1-reading
+		if (verbose) message("Reading dimensional vectors from ",file.in)		
+		con<-file(file.in,"rb") 
+		file.type<-readChar(con,4)
+		grid.id<-readBin(con,integer(),size=4)
+		minval<-readBin(con,numeric(),size=4)
+		maxval<-readBin(con,numeric(),size=4)
+		nlon<-readBin(con,integer(),size=4)
+		nlat<-readBin(con,integer(),size=4)
+		nlevel<-readBin(con,integer(),size=4)
+		t0.file<-readBin(con,numeric(),size=4)
+		tfin.file<-readBin(con,numeric(),size=4)
+
+		xlon<-array(0,c(nlat,nlon))
+		ylat<-array(0,c(nlat,nlon))
+		for(i in 1:nlat){
+			xlon[i,]<-readBin(con,numeric(),n=nlon,size=4)
+		}
+		for(i in 1:nlat){
+			ylat[i,]<-readBin(con,numeric(),n=nlon,size=4)
+		}
+		tvect<-readBin(con,numeric(),n=nlevel,size=4)
+
+		close(con)
+
+		return(list(t=tvect,x=xlon[1,],y=rev(ylat[,1])))
+	}
+	
+	
+	#Attn: need to get the date range before reading data, to avoid reading the whole array when it is not necessary!!!
+	#' Reading DYM file
+	#'
+	#' Reads 3d variable from the DYM file, which has dimensions (time,longitude,latitude), and returns the list with time vector, coordinates, land mask and variable data.
+	#' @param file.in the DYM file accessible with provided path
+	#' @param t0.user the date provided as c(year,month,day) to be used the first date for extraction. If not provided, then read starting the first date written in the DYM file.
+	#' @param tfin.user the date provided as c(year,month,day) to be used the last date for extraction. If not provided, read until the last time step written in the DYM file.
+	#' @param region the set of four values c(x1,x2,y1,y2) defining east-west and south-north coordinates respectively.
+	#' @param verbose flag (default value is TRUE) controlling the prompt for the nominal function execution.
+	#' @return The list of variables written in the DYM file, that is a vector of longitudes, x, and latitudes, y, pointing to the centers of grid cells, times, t (in Date format unless t.date.format set to FALSE), corresponding to the center of the time step, the land mask matrix, landmask and the data 3d matrices, var[t,x,y]. 
+	#' @examples 
+	#' data <- read.var.dym("sst.dym");
+	#' var<-data$var; tt<-data$t; x<-data$x; y<-data$y
+	#' @export
+	read.var.dym<-function(file.in,t0.user=NULL,tfin.user=NULL,region=c(NA,NA,NA,NA),dt=30,apply.mask=FALSE,t.date.format=TRUE,verbose=TRUE){
+
+		#Checking dates input
+		if (!is.null(t0.user)){
+			t0.user[2] <- ifelse(t0.user[2]<1,1,t0.user[2])
+			t0.user[2] <- ifelse(t0.user[2]>12,12,t0.user[2])
+			if (!is.null(tfin.user)){
+				tfin.user[2] <- ifelse(tfin.user[2]<1,1,tfin.user[2])
+				tfin.user[2] <- ifelse(tfin.user[2]>12,12,tfin.user[2])
+			}
+		}
+
+		#1-reading
+		if (verbose)
+			message("Reading file ",file.in,"...")		
+
+		con<-file(file.in,"rb") 
+		file.type<-readChar(con,4)
+		grid.id<-readBin(con,integer(),size=4)
+		minval<-readBin(con,numeric(),size=4)
+		maxval<-readBin(con,numeric(),size=4)
+		nlon<-readBin(con,integer(),size=4)
+		nlat<-readBin(con,integer(),size=4)
+		nlevel<-readBin(con,integer(),size=4)
+		t0.file<-readBin(con,numeric(),size=4)
+		tfin.file<-readBin(con,numeric(),size=4)
+
+		xlon<-array(0,c(nlat,nlon))
+		ylat<-array(0,c(nlat,nlon))
+		for(i in 1:nlat)
+			xlon[i,]<-readBin(con,numeric(),n=nlon,size=4)
+		
+		for(i in 1:nlat)
+			ylat[i,]<-readBin(con,numeric(),n=nlon,size=4)
+		
+		tvect<-readBin(con,numeric(),n=nlevel,size=4) 
+
+		mask<-array(0,c(nlat,nlon))
+		for(i in 1:nlat)
+			mask[i,]<-readBin(con,integer(),n=nlon,size=4)
+		
+			
+		#2-time vector
+		bytestoskip<-0
+
+		get.date.sea<-function(ndat){
+			year <- trunc(ndat)
+			days <- trunc((ndat - year)*365);
+			date <- ymd.2date(year,1,1) + days - 1
+			return(date)
+		}
+
+		get.dates <- function(){
+			if (dt==30)
+				return(gen.monthly.dates(year.month.sea(t0.file),year.month.sea(tfin.file)))
+			if (dt!=30)
+				return(get.date.sea(t0.file)+seq(0,(nlevel-1)*dt,dt))
+		}
+		
+		dates <- get.dates()
+		# IF extracting data for sub-time vector
+		if (!is.null(t0.user) | !is.null(tfin.user)){ 
+
+			if (is.null(t0.user))
+				t0.user <- date.2ymd(dates[1])
+			if (is.null(tfin.user))
+				tfin.user <- date.2ymd(dates[length(dates)])
+
+			#if day not specified add 15 in case of 30-days time stepping
+			if (dt == 30){
+				if (length(t0.user)==2) t0.user<-c(t0.user,15)
+				if (length(tfin.user)==2) tfin.user<-c(tfin.user,15)
+			} 
+			if (dt != 30){
+
+				#if day not specified in case of stadard calendar, add first and last of months
+				if (length(t0.user)==2){
+					message("Warning: the startdate does not contain day, will use the first of month!")
+					t0.user<-c(t0.user,1)
+				}
+
+				if (length(tfin.user)==2){
+					message("Warning: the enddate does not contain day, will use the last of month!")
+					if (tfin.user[2]<12)
+						last.in.month.date <- ymd.2date(tfin.user[1],tfin.user[2]+1,1)-1
+					if (tfin.user[2]==12)
+						last.in.month.date <- ymd.2date(tfin.user[1]+1,1,1)-1
+					
+					tfin.user <- c(tfin.user,as.integer(format(last.in.month.date,"%d")))
+				}
+			}
+			t0.user.date <- as.Date(paste(t0.user,collapse="-"))
+			tfin.user.date <- as.Date(paste(tfin.user,collapse="-"))
+
+			if (verbose){
+				message("User defined first date of extraction: ",t0.user.date)
+				message("User defined last date of extraction: ",tfin.user.date)
+			}
+
+			ind<-which(dates>=t0.user.date-.2*dt & dates<=tfin.user.date+.2*dt)
+		
+			if (length(ind)==0 | any(is.na(ind))){
+				message("Problem with dates! Quit now.")
+				print(ind)
+				return()
+			}
+			if (length(ind)>0 & all(!is.na(ind))){
+				dates<-dates[ind]
+				if (verbose)
+					message("Extracting data from ",dates[1]," to ",dates[length(dates)])			
+				tvect<-tvect[ind]	
+				bytestoskip<-(ind[1]-1)*nlon*nlat*4
+				nlevel<-length(tvect)
+			}
+		}
+		if (bytestoskip>0){
+			if (verbose)
+				message("Skipping ",bytestoskip/(nlon*nlat*4)," matrices...")		
+			pos<-seek(con,bytestoskip,"current")
+		}
+	
+		data<-array(0,c(nlevel+1,nlat,nlon))#'+1' to avoid dropping the first dimension
+	  	for(ti in 1:nlevel){
+			for(i in 1:nlat)
+				data[ti,i,]<-readBin(con,numeric(),n=nlon,size=4)
+		
+		}
+		#Convert invalid values in DYM to NA
+		#to avoid treating these values as valid ones
+		data<-ifelse(data==-999,NA,data)
+		  
+		close(con)
+
+		# IF extracting data for sub-region
+		if (!any(is.na(region))){
+			x1<-region[1]; x2<-region[2]		
+			y1<-region[3]; y2<-region[4]		
+			dx<-xlon[1,2]-xlon[1,1]
+			dy<-ylat[1,1]-ylat[2,1]
+			i1<-xtoi.dym(x1,xlon[1,1],dx)
+			i2<-xtoi.dym(x2,xlon[1,1],dx)
+			if (i1<1) i1<-1; if (i1>nlon) i1<-nlon
+			if (i2<1) i2<-1; if (i2>nlon) i2<-nlon
+			j2<-ytoj.dym(y1,ylat[1,1],dy)
+			j1<-ytoj.dym(y2,ylat[1,1],dy)
+			if (j1<1) j1<-1; if (j2>nlat) j2<-nlat
+			if (verbose)
+				message("Extracting data from ",xlon[1,i1]," to ",xlon[1,i2]," and from ",ylat[j2,1]," to ",ylat[j1,1])
+			mask<-mask[j1:j2,i1:i2,drop=F]
+			data<-data[,j1:j2,i1:i2,drop=F]
+			xlon<-xlon[j1:j2,i1:i2,drop=F]
+			ylat<-ylat[j1:j2,i1:i2,drop=F]; 
+		}
+		nlat<-nrow(ylat)
+	
+		#3-apply mask, flip and transpose
+		landmask.na<-ifelse(mask==0,NA,1)
+		if (apply.mask){		
+			#couldn't find how make 3d array from landmask.na to multiply data on. 
+			for (n in 1:length(dates)) data[n,,]<-data[n,,]*landmask.na 
+		}
+
+		data<-data[,nlat:1,,drop=F]
+		data<-apply(data,3:2,t)
+
+		data <- data[1:nlevel,,,drop=F]
+
+		if (!t.date.format) dates <- tvect
+	
+		return(list(x=xlon[1,],y=rev(ylat[,1]),t=dates,var=data,landmask=mask))
+	}
+
+	#' Reads restart DYM file
+	#'
+	#' Reads DYM file with population density initial conditions
+	#' @param file.in the name of the DYM file.
+	#' @examples 
+	#' dym <- read.restart.dym("skj_cohorts.dym");
+	#' summary(dym)
+	#' @export
+	read.restart.dym<-function(file.in){
+
+	#1-reading
+		message("Reading file ",file.in,"...")		
+	con<-file(file.in,"rb") 
+	file.type<-readChar(con,4)
+	grid.id<-readBin(con,integer(),size=4)
+	minval<-readBin(con,numeric(),size=4)
+	maxval<-readBin(con,numeric(),size=4)
+	nlon<-readBin(con,integer(),size=4)
+	nlat<-readBin(con,integer(),size=4)
+	nlevel<-readBin(con,integer(),size=4)
+	t0.file<-readBin(con,numeric(),size=4)
+	tfin.file<-readBin(con,numeric(),size=4)
+
+	xlon<-array(0,c(nlat,nlon))
+	ylat<-array(0,c(nlat,nlon))
+	for(i in 1:nlat){
+		xlon[i,]<-readBin(con,numeric(),n=nlon,size=4)
+	}
+	for(i in 1:nlat){
+		ylat[i,]<-readBin(con,numeric(),n=nlon,size=4)
+	}
+	tvect<-readBin(con,numeric(),n=nlevel,size=4) 
+
+	mask<-array(0,c(nlat,nlon))
+	for(i in 1:nlat){
+		mask[i,]<-readBin(con,integer(),n=nlon,size=4)
+	}
+			
+	data<-array(0,c(nlevel,nlat,nlon))
+	for(ti in 1:nlevel){
+		for(i in 1:nlat){
+		data[ti,i,]<-readBin(con,numeric(),n=nlon,size=4) 
+		}
+	}		
+	close(con)
+
+	nlat<-nrow(ylat)
+	data<-data[,nlat:1,]
+	data<-apply(data,3:2,t) 
+
+	return(list(x=xlon[1,],y=rev(ylat[,1]),var=data,landmask=mask))
+	}
+	
