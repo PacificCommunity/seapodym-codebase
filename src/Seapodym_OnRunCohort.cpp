1385a839cf081b038cd8375272be01ed4405e94f
innas@tiny <inna.senina@gmail.com>
2025-06-19 08:43:06 +0200

    Initial commit

---
+++ src/Seapodym_OnRunCohort.cpp
@@ -0,0 +1,414 @@
+#include "SeapodymCoupled.h"
+
+void update_density_like(dvar_matrix& Density_pred, const dmatrix density_input, const imatrix map_carte, const int nlon, const int nlat, const int nlon_input, const int nlat_input, dvariable& likelihood);
+void SeapodymCoupled::prerun_model()
+{
+	OnRunFirstStep();
+}
+
+///This is the main loop for the model without fishing and fitting of density. 
+///Similar to the default function, it includes the following calls:
+///1- Initialising population density 
+///2- Reading all forcing data (once in optimization mode, at every time step in simulation mode) 
+///3- Age/lifestage loop calling the ADRE solvers and ageing
+///4- Computing model density -> used as predictions
+///5- Likelihood computation using input density as observations
+///6- Writing outputs (in simulation mode only)
+///Note, there is no modelling of tagged cohorts here!
+
+//////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------//
+//		     FORAGE-TUNA SIMULATION			//
+//--------------------------------------------------------------//
+//////////////////////////////////////////////////////////////////
+/*!
+\brief The tuna population simulation without fishing and density fitting.
+*/
+double SeapodymCoupled::OnRunCohort(dvar_vector x, const bool writeoutputfiles)
+{
+	InitializeCohort();
+	past_month=month;
+	past_qtr=qtr;
+
+	//Temporarily reading the tau of the first cohort
+	//Need to be just a single number for all cohorts
+	int dtau = param->sp_unit_cohort[0][1];
+	int nbt_before_first_recruitment = Date::get_nbt_before_first_recruitment(
+			param->first_recruitment_date,
+			param->ndatini,param->deltaT,param->date_mode); 	
+	//counter of number of time steps between recruitments (survival equations)
+	//once nt_dtau = dtau, recruitment occurs and nt_dtau=0
+	//its initial value is dtau-nbt_before_first_recruitment_date
+	int nt_dtau = dtau-nbt_before_first_recruitment; 
+
+	//routine-specific variables
+	int tcur = t_count; //will be used for forcing variable time control
+	int nbt_no_forecast = t_count + nbt_spinup_tuna + nbt_total - 1;
+	bool fishing = false;
+	int migration_flag = 0;
+	int step_count= 0;
+	int step_fishery_count= 0;
+	int jday = 0; 
+	int nbstoskip = param->nbsteptoskip; // nb of time step to skip before computing likelihood
+
+	if (!param->gcalc()){
+		//need to read oxygen in case if month==past_month
+		//(otherwise we may not have it for the first time steps)
+		if (param->type_oxy==1 && month==past_month)
+			ReadClimatologyOxy(1, month);
+		//need to read oxygen in case if qtr==past_qtr 
+		if (param->type_oxy==2 && qtr==past_qtr)
+			ReadClimatologyOxy(1, qtr);
+	}
+
+	//----------------------------------------------//
+	// 	LIKELIHOOD INITIALISATION SECTION       //
+	//----------------------------------------------//	
+	double stocklike = 0.0;
+	dvariable likelihood = 0.0;
+	dvariable total_stock = 0.0;
+	//Reset model parameters:
+	reset(x);
+
+	//----------------------------------------------//
+	// 	LOCAL MATRICES ALLOCATION SECTION       //
+	//----------------------------------------------//	
+	dvar_matrix Spawning_Habitat;
+	dvar_matrix Total_pop;
+	dvar_matrix Habitat; 
+	dvar_matrix IFR; 
+	dvar_matrix ISR_denom; 
+	dvar_matrix FR_pop;
+	dvar_matrix Mortality; 
+	dvar_matrix dvarCohortDensity; 
+
+	Habitat.allocate(map.imin1, map.imax1, map.jinf1, map.jsup1);
+	Mortality.allocate(map.imin, map.imax, map.jinf, map.jsup);
+	Spawning_Habitat.allocate(map.imin, map.imax, map.jinf, map.jsup);
+	Total_pop.allocate(map.imin, map.imax, map.jinf, map.jsup);
+	dvarCohortDensity.allocate(map.imin1, map.imax1, map.jinf1, map.jsup1);
+
+	dvarCohortDensity = dvarDensity(sp,0);
+	int age = 0;
+
+	if (param->food_requirement_in_mortality(0)){ 
+		//temporal, need to check memory use first 
+		IFR.allocate(map.imin, map.imax, map.jinf, map.jsup);
+		ISR_denom.allocate(map.imin, map.imax, map.jinf, map.jsup);
+		FR_pop.allocate(map.imin, map.imax, map.jinf, map.jsup);
+		IFR.initialize();
+		ISR_denom.initialize();
+		FR_pop.initialize();
+	}
+	Spawning_Habitat.initialize();
+	Habitat.initialize();
+	Mortality.initialize();
+
+	//precompute thermal habitat parameters
+	for (int sp=0; sp < nb_species; sp++)
+		func.Vars_at_age_precomp(*param,sp);
+
+	//precompute seasonal switch function
+	for (int sp=0; sp < nb_species; sp++){
+		if (param->seasonal_migrations[sp]){
+			func.Seasonal_switch_year_precomp(*param,mat,map,
+						value(param->dvarsSpawning_season_peak[sp]),
+						value(param->dvarsSpawning_season_start[sp]),sp);
+		}
+	}
+
+	//Output DYM file name
+	string fileout;
+	fileout = param->strdir_output + param->sp_name[0] + "_cohort.dym";//will need the date of birth stamp
+	if (writeoutputfiles){
+		WriteFileHeaders_submodel(fileout,true);	
+		if (!param->gcalc())
+			ConsoleOutput(0,0);
+	}
+	//Vector of zeroes, to avoid duplicating the F_accessibility function 
+	ivector  tags_age_habitat;
+	tags_age_habitat.allocate(0,aN_adult(0));
+	tags_age_habitat.initialize();
+
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	////------------------------------------------------------------/////
+	////								/////
+	////		||| START OF SIMULATION CYCLE |||		/////
+	////								/////
+	////------------------------------------------------------------/////
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	for (;t_count <= nbt_total; t_count++)
+	{
+
+		//----------------------------------------------//
+		//              INITIALISATION                  //
+		//----------------------------------------------//
+		sumP=0;
+		sumFprime.initialize();
+		sumF.initialize();
+		mat.dvarCatch_est.initialize();
+		//----------------------------------------------//
+		//		       DATE			//
+		//----------------------------------------------//
+		getDate(jday);
+		for (int sp=0; sp < nb_species; sp++){
+			func.Seasonal_switch(*param,mat,map,jday,sp);	
+		}
+		tcur = t_count;
+		if (!param->gcalc()){
+			tcur = 1; 
+			//----------------------------------------------//
+			//	DATA READING SECTION: U,V,T,O2,PP	//
+			//----------------------------------------------//
+			//if ((t_count > nbt_building)) { //CC run with average effort forecast
+			if ((t_count > nbt_building) && (t_count <= nbt_no_forecast)) {
+				//TIME SERIES 
+				t_series = t_count - nbt_building + nbt_start_series;
+				ReadTimeSeriesData(tcur,t_series);	
+			}
+			else if (((t_count <= nbt_building) && (month != past_month)) || (t_count > nbt_no_forecast)) {
+	
+				//AVERAGED CLIMATOLOGY DATA
+				ReadClimatologyData(tcur, month);
+			}
+			if (param->type_oxy==1 && month != past_month) {
+				//MONTHLY O2
+				ReadClimatologyOxy(tcur, month);
+			}
+			if (param->type_oxy==2 && qtr != past_qtr) {
+				//QUARTERLY O2
+				ReadClimatologyOxy(tcur, qtr);
+			}
+		}
+		//------------------------------------------------------------------------------//
+		//	TRANSPORT OF TUNA AGE CLASSES AND PREDICTED CATCH COMPUTATION		//
+		//------------------------------------------------------------------------------//
+		//------------------------------------------------------------------------------//
+
+		for (int sp=0; sp < nb_species; sp++){
+if (age==0){
+			int elarvae_model = param->elarvae_model[sp];
+			//if !elarvae_model, elarvae_dt = 0 as elarvae_age = 0
+			elarvae_dt = param->elarvae_age[sp]/deltaT; 
+			double sigma_fcte_save = param->sigma_fcte;
+
+			//Precompute diagonal coefficients for larvae and juvenile ADREs
+			if (!elarvae_model){
+				mat.u = mat.un[tcur][0]; mat.v = mat.vn[tcur][0]; 
+				pop.precaldia(*param, map, mat);
+				pop.caldia(map, *param, mat.diffusion_x, mat.advection_x, mat.diffusion_y, mat.advection_y);
+			}
+}
+
+			//1. Precompute some variables outside of age loop
+			
+			//1.0 Forage scaling
+			func.Forage_Scaling(*param, mat, map, sp, tcur);
+
+//No need to precompute accessibility for all ages, can be done at each time step for a given age			
+			//1.1 Accessibility by adults (all cohorts)
+ 			func.Faccessibility(*param, mat, map, sp, jday, tcur, pop_built, false, tags_age_habitat);//checked
+
+			//1.6 Precompute Mortality range at age function
+			func.mortality_range_age_comp(*param,mat,sp);
+
+			//2. IMPLICIT AGE LOOP: increment age while moving through life stages 
+
+if (age==0){
+			//2.0 If activated, the Early Larvae Model (ELM) solved over elarvae_age
+			if (elarvae_model){
+				//Do the time-splitting for the first age class: 
+				//1- early (just a few days after yolk stage) and 
+				//2- late (up to one month of age) larvae
+		
+				bool time_getpred = false;
+				if (year>=param->larvae_like_firstyear
+						&& year<=param->larvae_like_lastyear 
+						&& t_count > nbt_building+nbstoskip)
+					time_getpred = true;
+
+				elarvae_model_run(Mortality,sp,tcur,time_getpred,writeoutputfiles);
+			}
+
+			//2.1 ADRE for late larvae in ELM or monthly larval class in default model 
+			//2.1.1 Spawning habitat
+			func.Spawning_Habitat(*param, mat, map, Spawning_Habitat, 1.0, sp, tcur, jday);
+			
+			//2.2 Transport and mortality of larvae (always one age class)	
+			for (int n=0; n<param->sp_nb_cohort_lv[sp]; n++){
+				double mean_age = mean_age_cohort[sp][age]; 
+
+				
+				func.Mortality_Sp(*param, mat, map, Mortality, Spawning_Habitat, sp, mean_age, age, tcur);
+				pop.Precalrec_juv(map, mat, Mortality, tcur, (1-elarvae_dt));//checked
+				pop.Calrec_juv(map, mat, mat.dvarCohortDensity, Mortality, tcur, (1-elarvae_dt));//checked
+				age++;
+			}
+
+			//2.2.0 Only in the ELM mode need to reset movement rates for juveniles
+			if (elarvae_model){
+				param->sigma_fcte = sigma_fcte_save;
+				mat.u = mat.un[tcur][0]; mat.v = mat.vn[tcur][0]; 
+				//Precompute diagonal coefficients for juvenile ADREs
+				pop.precaldia(*param, map, mat);
+				pop.caldia(map, *param, mat.diffusion_x, mat.advection_x, mat.diffusion_y, mat.advection_y);
+			}
+}
+
+if (age >0 && age <=param->sp_nb_cohort_jv[sp]){
+			//2.3. Juvenile habitat	
+			if (param->cannibalism[sp]){
+				Total_Pop_comp(Total_pop,sp,jday,tcur); //adjoint
+				func.Juvenile_Habitat_cannibalism(*param, mat, map, Habitat, Total_pop, sp, tcur);
+			} else 
+				func.Juvenile_Habitat(*param, mat, map, Habitat, sp, tcur);
+				
+			//2.4. Transport and mortality of juvenile age classes	
+				double mean_age = mean_age_cohort[sp][age];
+
+				func.Mortality_Sp(*param, mat, map, Mortality, Habitat, sp, mean_age, age, tcur);
+				pop.Precalrec_juv(map,  mat, Mortality, tcur, 1);
+				pop.Calrec_juv(map, mat, mat.dvarCohortDensity, Mortality, tcur, 1);
+}			
+if (age > param->sp_nb_cohort_jv[sp] && age <= param->sp_nb_cohort_jv[sp]+param->sp_nb_cohort_ad[sp]){
+			//4. Transport and mortality of adult cohort
+			for (int n=0; n<param->sp_nb_cohort_ad[sp]; n++){
+
+				//NOTE: currently current averaging doesn't depend on seasonal migrations
+				if (param->vert_movement[sp] && param->age_compute_habitat[sp][age]!=param->age_compute_habitat[sp][age-1]){
+
+					func.Average_currents(*param, mat, map, age, tcur, pop_built);
+				}
+					
+				//the option with smooth maturity to be revised and if necessary to be used later
+				if (param->migrations_by_maturity_flag && param->seasonal_migrations[sp]){
+					cout << "In this version no smooth maturity; enter the age at first maturity. Exit now!" << endl; exit(1);
+					
+				} // end of section with seasonality switch and <1 maturity at age parameter 
+				else {
+					migration_flag = 0;
+					if (age>=param->age_mature[sp] && param->seasonal_migrations[sp]) migration_flag = 1;
+
+					if (param->age_compute_habitat[sp][age]!=param->age_compute_habitat[sp][age-1]) {
+						func.Feeding_Habitat(*param,mat,map,Habitat,sp,age,jday,tcur,migration_flag);
+					}
+					double mean_age = mean_age_cohort[sp][age];
+
+					if (!param->food_requirement_in_mortality(sp)){
+						func.Mortality_Sp(*param, mat, map, Mortality, Habitat, sp, mean_age, age, tcur);//checked
+					} else {
+						Food_Requirement_Index(IFR, FR_pop, ISR_denom, sp, age, tcur, jday);
+						func.Mortality_Sp(*param, mat, map, Mortality, IFR, sp, mean_age, age, tcur);//checked
+					}
+
+					if (param->age_compute_habitat[sp][age]!=param->age_compute_habitat[sp][age-1]){
+						pop.Precaldia_Caldia(map, *param, mat, Habitat, Total_pop, sp, age, tcur, jday);//checked	
+					}
+					if (!param->gcalc()){
+						// Additional outputs:
+						// only in simulation mode: compute mean speed 
+						// in BL/sec and mean diffusion rate in nmi^2/day
+						mat.MeanVarMovement(map,mat.advection_x,
+							mat.advection_y,
+				 			value(mat.dvarsDiffusion_y),
+							param->MSS_species[sp],
+							param->sigma_species[sp],
+							param->length(sp,age),
+							param->length(sp,param->sp_nb_cohorts[sp]-1),
+							deltaT,sp,age);
+					}
+						
+					//store adult habitat and update the counter
+					if (param->age_compute_habitat[sp][age]!=param->age_compute_habitat[sp][age-1]){
+						//cout << age << " " << param->age_compute_habitat[sp][age] << endl;
+						mat.adult_habitat(sp,tcur,param->age_compute_habitat[sp][age]) = value(Habitat);
+					}
+					pop.Precalrec_Calrec_adult(map,mat,*param,rw,
+							mat.dvarCohortDensity,Mortality,
+							tcur,fishing,age,sp,year,month,
+							jday,step_fishery_count,0);//checked 20150210	
+				
+				}
+			
+		}//end of 'sp' loop
+
+		if (writeoutputfiles){
+			if (!param->gcalc())	
+				ConsoleOutput(1,value(likelihood));
+
+			dmatrix mat2d(0, nbi - 1, 0, nbj - 1);
+                        mat2d.initialize();
+                        for (int i=map.imin; i <= map.imax; i++){
+                                for (int j=map.jinf[i] ; j<=map.jsup[i] ; j++){
+                                        if (map.carte[i][j]){
+                                                mat2d(i-1,j-1) = value(dvarCohortDensity(i,j));
+                                        }
+                                }
+                        }
+			double minval=0.0;
+			double maxval=1.0;
+
+                        rw.wbin_transpomat2d(fileout, mat2d, nbi-2, nbj-2, true);
+			//update min-max values in header
+			rw.rwbin_minmax(fileout, minval, maxval);
+		}
+		nt_dtau++;
+		past_month=month;
+		step_count++;
+		if (qtr != past_qtr) past_qtr = qtr; 
+age++;
+	} // end of simulation loop
+
+	return 0;
+}
+
+//potentially to be moved to like.cpp
+void update_density_like(dvar_matrix& Density_pred, const dmatrix density_input, const imatrix map_carte, const int nlon, const int nlat, const int nlon_input, const int nlat_input, dvariable& likelihood){
+
+	int rr_x = (int)nlon/nlon_input; 
+	int rr_y = (int)nlat/nlat_input; 
+	
+	if (rr_x<1 || rr_y<1){ 
+		cerr << "Model resolution should be divisible without remainder by the resolution of input density field." << 
+		         endl<< "Currenly nlon/nlon_input = " << rr_x << ", and nlat/nlat_input = " << rr_y << endl << "Will exit now...";
+		exit(1);
+	}
+	dvariable Btot;
+	int cells_open;
+	for (int i=0; i<nlon_input; i++){
+		for (int j=0; j<nlat_input ; j++){
+			cells_open = 0;
+			for (int ii=0; ii<rr_x; ii++)
+			for (int jj=0; jj<rr_y; jj++){
+				if (map_carte(rr_x*i+ii,rr_y*j+jj))
+					cells_open++;
+			}
+
+			//non-zero density_input as data landmask and at least one cell in current model grid
+			if (density_input(i,j) && cells_open){
+				Btot = 0.0;
+				for (int ii=0; ii<rr_x; ii++)
+				for (int jj=0; jj<rr_y; jj++){
+					if (map_carte(rr_x*i+ii,rr_y*j+jj))
+					       	Btot += Density_pred[rr_x*i+ii][rr_y*j+jj];
+				}
+				
+				if (Btot>0)	 
+	                      		likelihood += (rr_x*rr_y*density_input(i,j)-Btot)*
+					      	      (rr_x*rr_y*density_input(i,j)-Btot);
+			}
+		}
+	}
+}
+
+void SeapodymCoupled::InitializeCohort()
+{
+        t_count = nbt_building+1;
+        mat.mats.initialize();
+        for (int sp=0; sp<nb_species; sp++){
+		mat.dvarDensity(sp,0) = mat.init_density_species(sp,0);
+        }
+}
+
---
+++ src/main_cohort.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using std::cout;
+void help(char* argv0);
+int OptionToCode(char* Option);
+int seapodym_cohort(const char* parfile, const int cmp_regime, const bool reset_buffers);
+bool read_memory_options(int argc, char** argv, const bool grad_calc);
+
+int main(int argc, char** argv) {
+	int cmp_regime = -1;
+	bool reset_buffers = false;
+	int k=1;
+	char *cmdLineOption = argv[k];
+	//Note, other options are yet to be integrated from the seapodym_coupled code
+	//Currently only simulation, optimization and Hessian calculation are possible
+	cmp_regime = OptionToCode(cmdLineOption);
+	if (cmp_regime==-1) k=0;
+	if (cmp_regime==-3) help(argv[0]);
+	if (argc < k+2) {
+		cout << "Too few parameters... \n"; 
+		help(argv[0]);
+	}
+	if ((cmp_regime==-1 && argc>2) || (cmp_regime>=0 && argc>3)){
+		bool grad_calc = false;
+		if (cmp_regime == -1 || cmp_regime == 2)
+			grad_calc = true;
+		reset_buffers = read_memory_options(argc, argv, grad_calc);	
+	}
+	return seapodym_cohort(argv[argc-1],cmp_regime,reset_buffers);
+}
+
+
+int OptionToCode(char* op) {
+
+	const int N = 12;
+	const char *cmdop[N] = {"-s","-p","-H","-t","-h","-v","--simulation","--likelihood-projection","--hessian","--taylor-test","--help","--version"};
+	int cmpCode[N] = {0,1,2,4,-3,-2,0,1,2,4,-3,-2};
+	for (int i=0; i<N; i++)
+		if (strcmp(op,cmdop[i])==0){
+			if (cmpCode[i]==-2) {
+				cout << "SEAPODYM 4.0 without fishing for parameter estimation using population density \n";
+				cout << "Copyright (C) 2022, SPC, CLS, University of Hawaii.\n";
+				exit(0);
+			}
+			return cmpCode[i];
+		}
+	return -1;//by default - optimization
+}
+
+void help(char* argv0) {
+
+	cout << "Usage:" << argv0 << " [option] parfile \n";
+	cout << "      IMPORTANT!!! If [option] is omitted, then application will start optimization run! \n"; 
+	cout << "Options: \n";
+	cout << "  -h, --help \t\t\t Print this message and exit.\n";
+	cout << "  -H, --hessian \t\t Compute Hessian matrix.\n";
+	cout << "  -t, --taylor-test   \t\t Perform Taylor derivative test with central differencing.\n";
+	cout << "  -s, --simulation \t\t Run simulation without optimization.\n";
+	cout << "  -v, --version \t\t Print version number and exit.\n";
+	exit(0);
+}
+
+
+
