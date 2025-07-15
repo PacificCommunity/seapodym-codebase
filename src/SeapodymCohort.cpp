c201272efc11362a3d46d274d22a70c000618e75
LucasBnnn <lucasb@spc.int>
2025-07-16 10:35:42 +1100

    Comment out traces for densities and cohort comparison

--- src/SeapodymCohort.cpp
+++ src/SeapodymCohort.cpp
@@ -277,7 +277,7 @@ void SeapodymCohort::stepForward(bool writeoutputfiles)
 			}
 		}
 	}//end of 'sp' loop
-	cerr << setprecision(8) << "t_count = " << t_count << ": sum(density) = " << sum(dvarCohortDensity) << endl;
+	//cerr << setprecision(8) << "t_count = " << t_count << ": sum(density) = " << sum(dvarCohortDensity) << endl;
 
 	if (writeoutputfiles){
 		//Output DYM file name
