e3fe39c70b63d673dc3f159dba9a47864f7fba45
Alex Pletzer <alexander@gokliya.net>
2026-03-23 16:31:09 +1300

    setting up run

---
+++ example-configs/density_skj_fat/plot_scaling.py
@@ -0,0 +1,69 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import defopt
+
+
+def main(*, input_file: str='results/timings.csv', output_file: str='results/scaling.png', show: bool=False):
+        """
+        input_file: CSV input file (e.g. results/timings.csv)
+        output_file: PNG output file
+        show: set to True to display
+        """
+
+        # Load CSV
+        df = pd.read_csv(input_file)
+
+
+        # Ensure manager_time column exists
+        if "manager_time" not in df.columns:
+            print(f"ERROR: 'manager_time' column not found in CSV. Cannot plot speedup.")
+
+        # Drop rows with missing manager_time
+        df = df.dropna(subset=["manager_time"])
+        if df.empty:
+            print(f"ERROR: No valid 'manager_time' values found. Cannot plot speedup.")
+        
+        # Plot curves for Milan & Genoa
+        cols = {
+ 		'milan': 'b', 
+ 		'genoa': 'r',
+ 		}
+        plt.figure(figsize=(8,6))
+        for host in df["host"].unique():
+
+
+            df_min = df[df.host == host].groupby(["nprocs"])["manager_time"].min().reset_index()
+            df_avg = df[df.host == host].groupby(["nprocs"])["manager_time"].mean().reset_index()
+            df_max = df[df.host == host].groupby(["nprocs"])["manager_time"].max().reset_index()
+
+            # Compute speedup relative to nprocs=2 for each host
+            df_min['speedup'] = df_min[ df_min["nprocs"] == 2 ]["manager_time"].to_numpy()[0] / df_max["manager_time"].to_numpy()   # note: divide by max
+            df_avg['speedup'] = df_avg[ df_min["nprocs"] == 2 ]["manager_time"].to_numpy()[0] / df_avg["manager_time"].to_numpy()
+            df_max['speedup'] = df_max[ df_min["nprocs"] == 2 ]["manager_time"].to_numpy()[0] / df_min["manager_time"].to_numpy()   # note: divide by min
+            
+            col = cols[host]
+            plt.plot(df_min["nprocs"], df_min["speedup"], col + '-.', label=host.capitalize() + ' min')
+            plt.plot(df_avg["nprocs"], df_avg["speedup"], col + '-', label=host.capitalize() + ' avg')
+            plt.plot(df_max["nprocs"], df_max["speedup"], col + '--', label=host.capitalize() + ' max')
+            
+            #print(df_max["nprocs"])
+            #print(df_max["speedup"])
+            
+        plt.plot([2,20], np.array([2,20]) - 1, 'k--', label='ideal')
+
+        plt.xlabel("Number of processes (nprocs)")
+        plt.ylabel("Speedup (relative to nprocs=2)")
+        plt.title("Seapodym cohort speedup")
+        plt.xticks(sorted(df["nprocs"].unique()))
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(output_file)
+        if show:
+            plt.show()
+        print(f"Speedup plot saved to {output_file}")
+        
+if __name__ == '__main__':
+	defopt.run(main)
