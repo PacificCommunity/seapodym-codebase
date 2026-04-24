b6e03072a75eec7a6e6c0da2793c82895bd4fb1d
Lucas Bonnin <lucasb@spc.int>
2026-04-24 15:47:03 +1100

    Update seapodym_fluxes with latest changes

--- src/main_simulation.cpp
+++ src/main_simulation.cpp
@@ -1,57 +1,12 @@
-#include <iostream>
-#include <cstring>
-#include <sys/stat.h>
-#include "ad_options.h"
-using std::cout;
+#include "Cmdopt.h"
 
-void help_sim(char* argv0);
-void CheckInfo(char* Option, char* argv0);
-int OptionToCode(char* Option,int &sub_option);
-int seapodym_coupled(const char* parfile, const int cmp_regime, const int sub_option, const int sub_option2, const bool reset_buffers);
-bool read_memory_options(int argc, char** argv, const bool grad_calc);
+int seapodym_coupled(const char* parfile, Cmdopt* cmdopt);
 
 int main(int argc, char** argv) {
 
-	if (argc==1) help_sim(argv[0]);
+	Cmdopt cmdopt(argc, argv);
 
-	char *cmdop = argv[1];
-	if (argc==2) CheckInfo(cmdop,argv[0]);
-
-	int cmp_regime = 0;
-	int sub_option = 0;
-	int sub_option2 = 0;
-	bool reset_buffers = false;
-	bool file_exists = false;
-
-	struct stat buffer;  
-	if (stat(argv[argc-1], &buffer)==0)
-		file_exists = true;
-
-	if (!file_exists){
-		cout << "The last argument must be existing parfile...\n";
-		help_sim(argv[0]);
-	}
-	if (argc>2 && file_exists) 
-		cmp_regime = OptionToCode(cmdop,sub_option);
-	if (cmp_regime==-1 || cmp_regime==-11 || cmp_regime==4){
-		cout << "\n!!!SEAPODYM without parameter estimation. Running in simulation mode!!!\n"; 
-		cmp_regime = 0;
-	}
-	if (cmp_regime==3 && sub_option==0)
-		sub_option = 1;
-
-	if (((cmp_regime==0 && argc>2) || (cmp_regime>=0 && argc>3)) && file_exists){
-		bool grad_calc = false;
-		if (cmp_regime == -1 || cmp_regime == 2)
-			grad_calc = true;
-		reset_buffers = read_memory_options(argc, argv, grad_calc);	
-	}
-
-	if (cmp_regime==3 && file_exists){
-		sub_option2 = read_sub_option2(argc, argv, cmp_regime);
-	}
-
-	return seapodym_coupled(argv[argc-1],cmp_regime,sub_option,sub_option2,reset_buffers);
+	return seapodym_coupled(argv[argc-1], &cmdopt);
 }
 
 
