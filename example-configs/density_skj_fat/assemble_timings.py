7ef28caad157a7d475691b7665002880bb15b608
Alex Pletzer <alexander@gokliya.net>
2026-06-10 10:16:36 +1200

    + assemble_timings.py

---
+++ example-configs/density_skj_fat/assemble_timings.py
@@ -0,0 +1,47 @@
+import re
+import glob
+import defopt
+import csv
+
+
+def main(*, dir : str='results', output : str='results/timings.csv'):
+    """
+    Assemble timings into a csv file
+    :param dir directory
+    """
+    pattern = re.compile(r"time manager\s*=\s*([0-9.]+)")
+    rows = []
+
+    for f in glob.glob(dir + '/run*.out'):
+        
+         print(f'working on {f}...')
+        
+         parts = f.split("/")[-1].replace(".out","").split("_")
+         n = int(parts[1])
+         r = int(parts[2])
+         host_branch = parts[3]
+         host, branch = host_branch.split('-')
+
+         jobid = None
+         manager_time = None
+
+         with open(f) as fh:
+            for line in fh:
+                if line.startswith("jobid="):
+                    jobid = line.split("=")[1].strip()
+                    m = pattern.search(line)
+                    if m:
+                        manager_time = float(m.group(1))
+
+            rows.append((n, jobid, host, branch, manager_time))
+
+         rows.sort()
+
+         with open(output, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["nprocs","jobid","host","branch", "manager_time"])
+            writer.writerows(rows)
+            
+if __name__ == '__main__':
+	defopt.run(main)
+
--- example-configs/density_skj_fat/run24genoa.sl
+++ example-configs/density_skj_fat/run24genoa.sl
@@ -4,12 +4,13 @@
 #SBATCH --partition=genoa
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
-#SBATCH --mem-per-cpu=4g
+##SBATCH --mem-per-cpu=4g
+#SBATCH --mem=10g
 #SBATCH --cpus-per-task=1
 #SBATCH --output=seapodym_%j.out
 #SBATCH --error=seapodym_%j.err
-##SBATCH --profile task
-##SBATCH --acctg-freq=1
+#SBATCH --profile task
+#SBATCH --acctg-freq=1
 #SBATCH --extra-node-info=1:*:1 
 #SBATCH --distribution=*:block:* 
 #SBATCH --mem-bind=local
@@ -20,4 +21,4 @@ module load gimkl/2020a
 rm log_*.txt
 
 # Run the MPI job
-srun ../../bin/seapodym_cohort -s skj_fat.xml
+srun ../../bin/seapodym_cohort-best -s skj_fat.xml
