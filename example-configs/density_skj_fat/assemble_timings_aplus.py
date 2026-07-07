0ff9085efebe04e2ff258136f826b0a4bb5acbcf
Alex Pletzer <alexander@gokliya.net>
2026-07-07 16:43:29 +1200

    plotting of A+ scalability

---
+++ example-configs/density_skj_fat/assemble_timings_aplus.py
@@ -0,0 +1,48 @@
+import re
+import glob
+import defopt
+import csv
+
+
+def main(*, dir : str='results', output : str='results/timings.csv'):
+    """
+    Assemble timings into a csv file
+    :param dir directory
+    """
+    pattern = re.compile(r"time manager\s*=\s*([0-9.]+)")
+    rows = []
+
+    for f in glob.glob(dir + '/run*.out'):
+        
+         print(f'working on {f}...')
+        
+         parts = f.split("/")[-1].replace(".out","").split("_")
+         n = int(parts[1])
+         r = int(parts[2])
+         host = parts[3]
+         case = parts[4]
+
+         jobid = None
+         manager_time = None
+
+         with open(f) as fh:
+            for line in fh:
+                if line.startswith("jobid="):
+                    jobid = line.split("=")[1].strip()
+                m = pattern.search(line)
+                if m:
+                    manager_time = float(m.group(1))
+                    print(f'manager time = {manager_time}')
+
+            rows.append((n, jobid, host, case, manager_time))
+
+         rows.sort()
+
+         with open(output, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["nprocs","jobid","host","case", "manager_time"])
+            writer.writerows(rows)
+            
+if __name__ == '__main__':
+	defopt.run(main)
+
---
+++ example-configs/density_skj_fat/plot_timings_aplus.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import defopt
+
+
+def main(*, input_file: str='results/timings.csv', output_file: str='results/timings.png', show: bool=False):
+        """
+        input_file: CSV input file (e.g. results/timings.csv)
+        output_file: PNG output file
+        show: set to True to display
+        """
+
+        # Load CSV
+        df = pd.read_csv(input_file)
+
+        # Ensure manager_time column exists
+        if "manager_time" not in df.columns:
+            print(f"ERROR: 'manager_time' column not found in CSV. Cannot plot speedup.")
+
+        # Drop rows with missing manager_time
+        df = df.dropna(subset=["manager_time"])
+        if df.empty:
+            print(f"ERROR: No valid 'manager_time' values found. Cannot plot speedup.")
+        
+        # Plot curves for Milan & Genoa
+        cols = {
+ 		'aplus': 'b', 
+ 		'aplus3': 'm',
+ 		}
+        plt.figure(figsize=(8,6))
+        for case in df["case"].unique():
+            df2 = df[df.case == case]
+            df_min = df2.groupby(["nprocs"])["manager_time"].min().reset_index()
+            df_avg = df2.groupby(["nprocs"])["manager_time"].mean().reset_index()
+            df_max = df2.groupby(["nprocs"])["manager_time"].max().reset_index()
+            
+            col = cols[case]
+            plt.loglog(df_min["nprocs"], df_min["manager_time"], col + '-.', label=case + ' min')
+            plt.loglog(df_avg["nprocs"], df_avg["manager_time"], col + '-', label=case + ' avg')
+            plt.loglog(df_max["nprocs"], df_max["manager_time"], col + '--', label=case + ' max')
+                        
+        plt.loglog([1,100], 1000./np.array([1,100]), 'k-', label='ideal')
+
+        plt.xlabel("Number of processes (nprocs)")
+        plt.ylabel("Speedup (relative to nprocs=2)")
+        plt.title("Seapodym cohort timings 1983-2022 skj_Fat.xml A+")
+        plt.xticks(sorted(df["nprocs"].unique()))
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(output_file)
+        if show:
+            plt.show()
+        print(f"plot saved to {output_file}")
+        
+if __name__ == '__main__':
+	defopt.run(main)
